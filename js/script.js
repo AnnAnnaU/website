ce65d373bd10ab3631ddf177b458e7636f2dcab9
AnnAnnaU <62073766+AnnAnnaU@users.noreply.github.com>
2020-03-30 13:25:32 +0300

    website

--- js/script.js
+++ js/script.js
@@ -103,14 +103,53 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
     });
 
+  // Form
+
+  let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Мы с Вами свяжемся!',
+    failure: 'Что-то пошло не так...'
+};
+
+let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    form.appendChild(statusMessage);
+
+    let request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+    request.
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+    let formData = new FormData(form);
+
+    let obj = {};
+    formData.forEach(function(value, key) {
+        obj[key] = value;
+    });
+    let json = JSON.stringify(obj);
+
+    request.send(json);
+
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState < 4) {
+            statusMessage.innerHTML = message.loading;
+        } else if(request.readyState === 4 && request.status == 200) {
+            statusMessage.innerHTML = message.success;
+        } else {
+            statusMessage.innerHTML = message.failure;
+        }
+    });
+
+    for (let i = 0; i < input.length; i++) {
+        input[i].value = '';
+    }
 });
 
-// Р’С‚РѕСЂРѕРµ Р·Р°РґР°РЅРёРµ
 
-// let age = document.getElementById('age');
- 
-// function showUser(surname, name) {
-//          alert("РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ " + surname + " " + name + ", РµРіРѕ РІРѕР·СЂР°СЃС‚ " + this.value);
-// }
- 
-// showUser.apply(age, ["Р“РѕСЂСЊРєРёР№","РњР°РєСЃРёРј"]);
+});
